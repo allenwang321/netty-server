30014bccd9316d9b3bb89f5c8b9027414d520f4a
allenwang <zbxhui@gmail.com>
2019-06-27 09:59:32 +0800

    客户端服务端完成登录

---
+++ src/main/java/com/bestboke/nettyserver/learn3/ServerHandler.java
@@ -0,0 +1,36 @@
+package com.bestboke.nettyserver.learn3;
+
+import com.bestboke.nettycommons.nettypacket.LoginRequestPacket;
+import com.bestboke.nettycommons.nettypacket.LoginResponsePacket;
+import com.bestboke.nettycommons.nettypacket.Packet;
+import com.bestboke.nettycommons.nettyserializer.PacketCodeC;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf requestByteBuf = (ByteBuf) msg;
+
+        Packet packet = PacketCodeC.INSTANCE.decode(requestByteBuf);
+
+        LoginResponsePacket loginResponsePacket = new LoginResponsePacket();
+        if (packet instanceof LoginRequestPacket) {
+            LoginRequestPacket loginRequestPacket = (LoginRequestPacket) packet;
+            if (valid(loginRequestPacket)) {
+                loginResponsePacket.setSuccess(true);
+            } else {
+                loginResponsePacket.setSuccess(false);
+                loginResponsePacket.setReason("账号密码校验失败");
+            }
+        }
+        ByteBuf responseByteBuf = PacketCodeC.INSTANCE.encode(ctx.alloc(), loginResponsePacket);
+        ctx.channel().writeAndFlush(responseByteBuf);
+    }
+
+    private Boolean valid(LoginRequestPacket loginRequestPacket) {
+        return true;
+    }
+
+}
